6e6fc4ac7b06b571aea912da59f714904b909480
MstyslavDmytryk <mstyslavdmytryk@gmail.com>
2023-02-08 13:10:50 +0200

    webpack config and feature-slice design

--- config/build/buildResolvers.ts
+++ config/build/buildResolvers.ts
@@ -1,8 +1,12 @@
+import { BuildOptions } from "./types/config";
 import { ResolveOptions } from "webpack";
 
-export function buildResolvers(): ResolveOptions {
-
-    return {
-        extensions: [".tsx", ".ts", ".js"],
-      }
+export function buildResolvers(options: BuildOptions): ResolveOptions {
+  return {
+    extensions: [".tsx", ".ts", ".js"],
+    preferAbsolute: true,
+    modules: [options.paths.src, "node_modules"],
+    mainFiles: ["index"],
+    alias: {},
+  };
 }
--- webpack.config.ts
+++ webpack.config.ts
@@ -1,28 +1,27 @@
-import { BuildPath, BuildEnv } from './config/build/types/config';
-import webpack from 'webpack'
+import { BuildPath, BuildEnv } from "./config/build/types/config";
+import webpack from "webpack";
 import { buildWebpackConfig } from "./config/build/buildWebpackConfig";
-import path from 'path';
-
+import path from "path";
 
 export default (env: BuildEnv) => {
-
   const paths: BuildPath = {
     entry: path.resolve(__dirname, "src", "index.tsx"),
     build: path.resolve(__dirname, "build"),
     html: path.resolve(__dirname, "public", "index.html"),
-  }
-  
-  const mode = env.mode || 'development'
+    src: path.resolve(__dirname, "src"),
+  };
+
+  const mode = env.mode || "development";
   const PORT = env.port || 3000;
 
-  const isDev = mode === 'development'
-  
+  const isDev = mode === "development";
+
   const config: webpack.Configuration = buildWebpackConfig({
     mode,
     paths,
     isDev,
     port: PORT,
-  })
+  });
 
-  return config
-}
+  return config;
+};
